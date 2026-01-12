cfa43371f39049f1bd6df8509d2103146540615f
Spring2LEE <ljs508077@gmail.com>
2026-01-12 13:44:30 +0900

    Remove password field from UpdateResponse
    Removed password field from UpdateResponse DTO

--- src/main/java/com/jpa/dto/UpdateResponse.java
+++ src/main/java/com/jpa/dto/UpdateResponse.java
@@ -10,13 +10,13 @@ public class UpdateResponse {
     private final String title;
     private final String content;
     private final String name;
-    private final String password;
+ 
 
     public UpdateResponse(long id, String title, String content, String name, String password) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.name = name;
-        this.password = password;
+        
     }
 }
